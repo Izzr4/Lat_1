3cc07b93350ce9618e0a515d59fdaca42774f44b
Izzr4 <112739379+Izzr4@users.noreply.github.com>
2022-10-05 17:49:13 +0700

    Add files via upload

---
+++ untitled/src/Bioskop.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Bioskop
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int org = input.nextInt();
+        int hari = input.nextInt();
+        int jam = input.nextInt();
+        int hgawal = 20000, harga = 2;
+        float diskon = 0.05f;
+
+        if ((org < 1 || org > 100) || (hari< 1 || hari >7) || (jam < 0 || jam > 24))
+        {
+            System.out.println("Inputan tidak valid");
+            System.exit(0);
+        }
+        if (hari < 6)
+        {
+            if (jam >= 0 && jam<=11)
+            {
+                hgawal += 0;
+            }else
+            {
+                hgawal += 10000;
+                System.out.println(hgawal);
+            }
+        }
+        else if (hari >= 6) {
+            if (jam >= 0 && jam <= 11)
+            {
+                hgawal += 15000;
+            }
+            else
+            {
+                hgawal += 20000;
+            }
+        }
+
+        if (org > 2)
+        {
+            harga = (int) (hgawal-(hgawal*diskon));
+            System.out.println("Total : Rp "+(harga*org));
+        }else
+        {
+            System.out.println("Total : Rp "+(hgawal*org));
+        }
+
+    }
+}
